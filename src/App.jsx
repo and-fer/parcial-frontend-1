3e57a305af2a1fa64b8f06e2b98671f166764425
Andres Ferrada <an.ferrada@gmail.com>
2024-11-07 00:39:43 -0300

    Agregar primera configuración de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import Card from './Card'
+
+function App() {
+   return (
+      <>
+         <h1>Carga de estudiantes</h1>
+         <form></form>
+         <Card test='y yo soy un prop...' />
+      </>
+   )
+}
+
+export default App
---
+++ src/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = ({ test }) => {
+   return (
+      <>
+         <div>Hola, soy una tarjeta</div>
+         <div>{test}</div>
+      </>
+   )
+}
+
+export default Card
